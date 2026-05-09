078c207224cb50daac7e295ded27e368bd0b6512
Dawid Mackiewicz <dawid2004m@gmail.com>
2026-05-09 02:06:18 +0200

    improved front end

---
+++ Kaucjomat/Behavior/DecimalValidatorBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Kaucjomat.Behavior
+{
+    public class DecimalValidatorBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(e.NewTextValue))
+                return;
+
+            var regex = new Regex(@"^\d*[,.]?\d{0,2}$");
+
+            if (!regex.IsMatch(e.NewTextValue))
+            {
+                ((Entry)sender).Text = e.OldTextValue;
+            }
+        }
+    }
+}
